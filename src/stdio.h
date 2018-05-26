47c70de3633803d4d8cea7cd701a82fd74ee1ef6
Jon Craton <jncraton@gmail.com>
2018-05-26 14:01:04 -0400

    WIP read syscall

--- src/stdio.h
+++ src/stdio.h
@@ -5,6 +5,19 @@
 void write(void* fd, char* buf, long len) {
   asm ("mov $1,%%rax;" // Write is syscall 1
        "mov $1,%%rdi;" // stdout is file descriptor 1
+       "mov %0,%%rsi;" // memory location to read
+       "mov %1,%%rdx;" // length
+       "mov %%r8,%%r10;"
+       "mov %%r9,%%r8;"
+       "syscall;"
+       :
+       :"r"(buf),"r"(len)  /* 11 is input operand and it's related to %0 */
+       :"rax","rdi","rsi","rdx","rcx","r8","r9","r10","r11"); /* clobbered registers */  
+}
+
+long read(void* fd, char* buf, long len) {
+  asm ("mov $0,%%rax;" // Write is syscall 0
+       "mov $0,%%rdi;" // stdin is file descriptor 0
        "mov %0,%%rsi;" // memory location to write
        "mov %1,%%rdx;" // length
        "mov %%r8,%%r10;"
@@ -12,5 +25,7 @@ void write(void* fd, char* buf, long len) {
        "syscall;"
        :
        :"r"(buf),"r"(len)  /* 11 is input operand and it's related to %0 */
-       :"rax","rdi","rsi","rdx"); /* clobbered registers */  
+       :"rax","rdi","rsi","rdx","rcx","r8","r9","r10","r11"); /* clobbered registers */
+
+  return 14;
 }
