5e3cdae950b297275b9c2ecf236e5c23733b6982
Jon Craton <jncraton@gmail.com>
2018-05-26 14:33:41 -0400

    Cleaned up system calls

--- src/stdio.h
+++ src/stdio.h
@@ -2,39 +2,31 @@
 #define size_t long
 #define ssize_t signed long
 
-#define stdin (void*)0
-#define stdout (void*)1
-#define stderr (void*)2
+#define stdin 0
+#define stdout 1
+#define stderr 2
 
-ssize_t write(void* fd, char* buf, size_t len) {
-  asm ("mov $1,%%rax;" // Write is syscall 1
-       "mov $1,%%rdi;" // stdout is file descriptor 1
-       "mov %0,%%rsi;" // memory location to read
-       "mov %1,%%rdx;" // length
+ssize_t syscall3(size_t syscall, size_t a, size_t b, size_t c) {
+  // https://filippo.io/linux-syscall-table/
+  asm ("mov %0,%%rax;" // System call number
+       "mov %1,%%rdi;" // arg1
+       "mov %2,%%rsi;" // arg2
+       "mov %3,%%rdx;" // arg3
        "mov %%r8,%%r10;"
        "mov %%r9,%%r8;"
        "syscall;"
        :
-       :"r"(buf),"r"(len)  /* 11 is input operand and it's related to %0 */
-       :"rax","rdi","rsi","rdx","rcx","r8","r9","r10","r11"); /* clobbered registers */  
+       :"r"(syscall),"r"(a),"r"(b),"r"(c)
+       :"rax","rdi","rsi","rdx","r8","r9","r10","rcx","r11"); /* clobbered registers */
 
-  return len;
+  register int ret asm("rax");
+  return ret;
 }
 
-ssize_t read(void* fd, char* buf, size_t len) {
-  ssize_t bytes_read;
+ssize_t write(size_t fd, char* buf, size_t len) {
+  return syscall3(1, fd, (size_t)buf, len);
+}
 
-  asm ("mov $0,%%rax;" // Write is syscall 0
-       "mov $0,%%rdi;" // stdin is file descriptor 0
-       "mov %1,%%rsi;" // memory location to write
-       "mov %2,%%rdx;" // length
-       "mov %%r8,%%r10;"
-       "mov %%r9,%%r8;"
-       "syscall;"
-       "mov %%rax,%0;"
-       :"=r"(bytes_read)
-       :"r"(buf),"r"(len)  /* 11 is input operand and it's related to %0 */
-       :"rax","rdi","rsi","rdx","rcx","r8","r9","r10","r11"); /* clobbered registers */
-
-  return bytes_read;
+ssize_t read(size_t fd, char* buf, size_t len) {
+  return syscall3(0, fd, (size_t)buf, len);
 }
