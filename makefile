e479657c189a0373a2d2c0a11af640776b531b72
Jon Craton <jncraton@gmail.com>
2018-05-26 13:18:56 -0400

    Working 'Hello world

--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 all: qkstats
 
 qkstats: src/main.c
-	gcc -o qkstats -Wall -Werror -funsigned-char -static -Os -nostdlib -Wl,--gc-sections -fno-unwind-tables -fno-asynchronous-unwind-tables src/main.c 
+	gcc -m64 -o qkstats -Wall -Werror -funsigned-char -static -Os -nostdlib -Wl,--gc-sections -fno-unwind-tables -fno-asynchronous-unwind-tables src/main.c 
 	strip qkstats
 	cat qkstats | wc
 
--- src/main.c
+++ src/main.c
@@ -1,7 +1,20 @@
 void _start() {
+    const char* msg = "Hello, World!\n";
+    long len = 14;
+
+    asm ("mov $1,%%rax;" // Write is syscall 1
+         "mov $1,%%rdi;" // stdout is file descriptor 1
+         "mov %0,%%rsi;" // memory location to write
+         "mov %1,%%rdx;" // length
+         "mov %%r8,%%r10;"
+         "mov %%r9,%%r8;"
+         "syscall;"
+         :
+         :"r"(msg),"r"(len)  /* 11 is input operand and it's related to %0 */
+         :"rax","rdi","rsi","rdx"); /* %eax is clobbered register */  
+
   asm("movl $1,%eax;"
       "xorl %ebx,%ebx;"
       "int  $0x80"
   );
 }
-
