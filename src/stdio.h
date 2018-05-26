724c60d1872ec844a8244cf52145e5029d44d4f6
Jon Craton <jncraton@gmail.com>
2018-05-26 15:07:55 -0400

    Use macros for system call implentations

--- src/stdio.h
+++ src/stdio.h
@@ -6,7 +6,18 @@
 #define stdout 1
 #define stderr 2
 
-ssize_t syscall3(size_t syscall, size_t a, size_t b, size_t c) {
+ssize_t syscall3(size_t sysno, size_t a, size_t b, size_t c) {
+  /*
+  size_t* arg = &sysno;
+
+  size_t args[5];
+  for (int i = 0; i < 5; i++) {
+    arg++;
+    args[i] = *arg;
+  }
+  */
+  
+  // implements a 3 parameter system call
   // https://filippo.io/linux-syscall-table/
   asm ("mov %0,%%rax;" // System call number
        "mov %1,%%rdi;" // arg1
@@ -16,17 +27,12 @@ ssize_t syscall3(size_t syscall, size_t a, size_t b, size_t c) {
        "mov %%r9,%%r8;"
        "syscall;"
        :
-       :"r"(syscall),"r"(a),"r"(b),"r"(c)
+       :"r"(sysno),"r"(a),"r"(b),"r"(c)
        :"rax","rdi","rsi","rdx","r8","r9","r10","rcx","r11"); /* clobbered registers */
 
   register int ret asm("rax");
   return ret;
 }
 
-ssize_t write(size_t fd, char* buf, size_t len) {
-  return syscall3(1, fd, (size_t)buf, len);
-}
-
-ssize_t read(size_t fd, char* buf, size_t len) {
-  return syscall3(0, fd, (size_t)buf, len);
-}
+#define read(fd,buf,len) syscall3(0,fd,(size_t)buf,len)
+#define write(fd,buf,len) syscall3(1,fd,(size_t)buf,len)
