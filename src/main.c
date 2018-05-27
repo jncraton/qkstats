f5dc535a5d8906babe543249fe4d6aa0dafd4079
Jon Craton <jncraton@gmail.com>
2018-05-27 00:38:45 -0400

    Display I/O read requests to sda

--- src/main.c
+++ src/main.c
@@ -3,30 +3,21 @@
 int main() {
   char buf[65535];
 
-  sprintf(buf, "Some numbers: %d %d %d %d\n", 42, 100, 200, 675);
-  write(stdout, buf, strlen(buf));
-
   FILE* fp = open("/sys/block/sda/stat",O_RDONLY);
-  size_t len = read(fp, buf, 65535);
+  read(fp, buf, 65535);
   close(fp);
-  write(stdout, buf, len);
 
-  /*
-        if stat == 'requests':
-          tot[0] += int(stats[0]) # Read requests
-          tot[1] += int(stats[4]) # Write requests
-        elif stat == 'in_flight':
-          tot[0] += int(stats[8])
-        elif stat == 'ticks':
-          tot[0] += int(stats[9]) / 1000.0
-        elif stat == 'wait_time':
-          tot[0] += int(stats[10])
-        elif stat == 'sectors':
-          tot[0] += int(stats[2])
-          tot[1] += int(stats[6])
-        elif stat == 'megabytes':
-          tot[0] += int(stats[2]) * 512 / 1024 / 1024 # Sectors read to megabytes
-          tot[1] += int(stats[6]) * 512 / 1024 / 1024 # Sectors written to megabytes
+  int start = 0;
+  int end;
+  for (start=0;*(buf+start)==' ';start++);
+  for (end=start;*(buf+end)!=' ';end++);
+
+  size_t read_reqs = strtol(buf+start,buf+end-1,10);
   
-  */
+  for (int i = 0; i<65535; i++) {
+    buf[i] = 0;
+  }
+  
+  sprintf(buf, "Read requests: %d\n", (int)read_reqs);
+  write(stdout, buf, strlen(buf));
 }
