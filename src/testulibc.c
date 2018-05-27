5e905e6679ab302aa838be23e220c0b71a064f73
Jon Craton <jncraton@gmail.com>
2018-05-27 17:36:18 -0400

    Cleaned up strchr tests

--- src/testulibc.c
+++ src/testulibc.c
@@ -3,14 +3,13 @@
 int main() {
   assert(strlen("")==0);
   assert(strlen("1234")==4);
-  
-  assert(strchr("aaa",(int)'b')==(char*)0x00);
-  assert(strchr("a b c",' ')==(char*)&("a b c")+1);
-  assert(strchr(" ",'a')==0x00);
-  assert(strchr("ab",'b')==(char*)&("ab")+1);
-  assert(strchr("aaaba",'b')==(char*)&("aaaba")+3);
 
   char buf[16] = "   a b, c";
+  
+  assert(strchr(buf,'d')==0);
+  assert(strchr(buf,' ')==buf);
+  assert(strchr(buf,'a')==buf+3);
+  assert(strchr(buf,'b')==buf+5);
 
   assert(*strtok(buf, " ") == 'a');
   assert(*strtok(0x00, " ") == 'b');
