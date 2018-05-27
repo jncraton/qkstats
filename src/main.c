d87711a4c680707187010f666315f05aff7986f8
Jon Craton <jncraton@gmail.com>
2018-05-27 17:08:01 -0400

    Display all fields

--- src/main.c
+++ src/main.c
@@ -1,22 +1,27 @@
 #include "ulibc.h"
 
 int main() {
-  char buf[65535];
+  char blockdev[255];
 
   FILE* fp = open("/sys/block/sda/stat",O_RDONLY);
-  read(fp, buf, 65535);
+  read(fp, blockdev, 255);
   close(fp);
 
-  write(stdout,buf,strlen(buf));
-  char* tok = strtok(buf,"\t ");
+  write(stdout,blockdev,strlen(blockdev));
 
-  write(stdout,tok,strlen(tok));
-  size_t read_reqs = atol(tok);
-  
-  for (int i = 0; i<65535; i++) {
-    buf[i] = 0;
+
+  char buf[255];  
+  int field = 0;
+
+  char* tok = strtok(blockdev,"\t ");
+  while (tok) {
+    for (int i = 0; i<255; i++) {
+      buf[i] = 0;
+    }
+    
+    sprintf(buf, "%d: %d\n", field, (int)atol(tok));
+    write(stdout, buf, strlen(buf));
+    field++;
+    tok = strtok(0x00,"\t ");
   }
-  
-  sprintf(buf, "Read requests: %d\n", (int)read_reqs);
-  write(stdout, buf, strlen(buf));
 }
